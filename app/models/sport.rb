a520d6f7f7d8aff06455a8c890ba62e6c5d0c0c7
Ubuntu <ubuntu@ip-172-31-37-57.ec2.internal>
2015-03-20 00:48:51 +0000

    add a sport model

---
+++ app/models/sport.rb
@@ -0,0 +1,3 @@
+class Sport < ActiveRecord::Base
+  validates_presence_of :name
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150319205614) do
+ActiveRecord::Schema.define(version: 20150320004735) do
 
   create_table "players", force: :cascade do |t|
     t.integer  "player_id"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 20150319205614) do
     t.string   "last_name"
     t.string   "position"
     t.integer  "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sports", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
