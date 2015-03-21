f50ee9a3e221d29a8f0dab3954ab4a314eb43e82
Ubuntu <ubuntu@ip-172-31-37-57.ec2.internal>
2015-03-21 05:24:57 +0000

    remove a bunch of unnecessary files

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,5 +1,5 @@
 class PlayersController < ApplicationController
-  before_action :set_player, only: [:show, :edit, :update, :destroy]
+  before_action :set_player, only: [:show]
   before_action :set_default_response_format
 
   # GET /players
@@ -45,59 +45,11 @@ class PlayersController < ApplicationController
   def show
   end
 
-  # GET /players/new
-  def new
-    @player = Player.new
-  end
-
-  # GET /players/1/edit
-  def edit
-  end
-
-  # POST /players
-  # POST /players.json
-  def create
-    @player = Player.new(player_params)
-
-    respond_to do |format|
-      if @player.save
-        format.html { redirect_to @player, notice: 'Player was successfully created.' }
-        format.json { render :show, status: :created, location: @player }
-      else
-        format.html { render :new }
-        format.json { render json: @player.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /players/1
-  # PATCH/PUT /players/1.json
-  def update
-    respond_to do |format|
-      if @player.update(player_params)
-        format.html { redirect_to @player, notice: 'Player was successfully updated.' }
-        format.json { render :show, status: :ok, location: @player }
-      else
-        format.html { render :edit }
-        format.json { render json: @player.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /players/1
-  # DELETE /players/1.json
-  def destroy
-    @player.destroy
-    respond_to do |format|
-      format.html { redirect_to players_url, notice: 'Player was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   protected
 
+  # Always return json
   def set_default_response_format
-    request.format = :json unless params[:format]
+    request.format = :json
   end
 
   private
--- spec/views/players/index.html.erb_spec.rb
+++ spec/views/players/index.html.erb_spec.rb
@@ -24,11 +24,6 @@ RSpec.describe "players/index", type: :view do
 
   it "renders a list of players" do
     render
-    assert_select "tr>td", :text => 1.to_s, :count => 2
-    assert_select "tr>td", :text => "Sport".to_s, :count => 2
-    assert_select "tr>td", :text => "First Name".to_s, :count => 2
-    assert_select "tr>td", :text => "Last Name".to_s, :count => 2
-    assert_select "tr>td", :text => "Position".to_s, :count => 2
-    assert_select "tr>td", :text => 2.to_s, :count => 2
+    raise 'implement'
   end
 end
